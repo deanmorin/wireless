a868f92616fe9510d384e68582157b90c97f4067
Dean Morin <morin.dean@gmail.com>
2010-11-13 19:59:03 -0800

    added the datalink files

---
+++ DataLink.c
@@ -0,0 +1,12 @@
+#include "DataLink.h"
+
+INT GetStateEvents(HANDLE* hEvents, INT state) {
+    
+    switch (state) {
+        case STATE_IDLE:
+        case STATE_T1:
+        case STATE_T3:
+        case STATE_R2:
+            break;
+    }
+}
