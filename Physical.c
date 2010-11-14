d8c11625494c374785215a08567df324fade2094
unknown <Ian@.(none)>
2010-11-13 19:35:00 -0800

    added more structure to read thread

--- Physical.c
+++ Physical.c
@@ -85,11 +85,11 @@ DWORD WINAPI ReadThreadProc(HWND hWnd) {
 
 	
     while (pwd->bConnected) {
-		
+		/*
 		if(!requestPending){
 			RequestPacket(hWnd);
 			requestPending = TRUE;
-		}
+		}*/
 
         SetCommMask(pwd->hPort, EV_RXCHAR);
         if (!WaitCommEvent(pwd->hPort, &dwEvent, &overlap)) {
@@ -113,21 +113,56 @@ DWORD WINAPI ReadThreadProc(HWND hWnd) {
             }
 
             dwQueueSize = AddToBack(&pHead, psReadBuf, dwBytesRead);
-            if (dwQueueSize >= 2) {
-               /* dwPacketLength = GetFromList(pHead, 2);*/
-				dwPacketLength = 1024;//ian
-
             
-                if (dwQueueSize >= dwPacketLength) {
+			/*if (dwQueueSize >= 2) {*/
+               /* dwPacketLength = GetFromList(pHead, 2);*/
+			switch(GetFromList(pHead,1)){
+				case 0x1://SOH  ie a frame.
+					dwPacketLength = 1024;
+					if (dwQueueSize >= dwPacketLength) {
 
-                    pcPacket = RemoveFromFront(&pHead, dwPacketLength);
-				    ProcessPacket(hWnd, pcPacket, dwPacketLength);
-                    memset(psReadBuf, 0, READ_BUFSIZE);
-				    requestPending = FALSE;
-                    free(pcPacket);
-			    }
+						pcPacket = RemoveFromFront(&pHead, dwPacketLength);
+						ProcessPacket(hWnd, pcPacket, dwPacketLength);
+						memset(psReadBuf, 0, READ_BUFSIZE);
+						/*requestPending = FALSE;*/
+						free(pcPacket);
+					}
+
+					break;
+				case 0x4://EOT
+					dwPacketLength = 1;
+					//process EOT
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+				case 0x5://ENQ
+					dwPacketLength = 1;
+					//process ENQ
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+				case 0x6://ACK0
+					dwPacketLength = 1;
+					//process ACK0
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+				case 0x11://ACK1
+					dwPacketLength = 1;
+					//process ACK1
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+				case 0x12://WACK
+					dwPacketLength = 1;
+					//process WACK
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+				case 0x13://RVI
+					dwPacketLength = 1;
+					//process RVI
+					RemoveFromFront(&pHead, dwPacketLength);
+					break;
+			}
+			
                 InvalidateRect(hWnd, NULL, FALSE);
-            }
+            /*}*/
         }
         ResetEvent(overlap.hEvent);
     }
