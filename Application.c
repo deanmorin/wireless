5fdfa7f01973f4c96cf07957f560237970086eec
Marcel <marcel@Galactus>
2010-11-27 13:06:14 -0800

    took out timer using messages for dialog boxes

--- Application.c
+++ Application.c
@@ -616,6 +616,7 @@ VOID UpdateStatStruct(HWND hWnd, WPARAM stat, LPARAM attribute) {
 
         case STAT_STATE:
             DL_STATE = attribute;
+			SendMessage(pwd->hDlgDebug, WM_USER, 0, 0);
             break;
         
         case ACK:
@@ -662,4 +663,5 @@ VOID UpdateStatStruct(HWND hWnd, WPARAM stat, LPARAM attribute) {
             NUM_FILES++;
             break;
     }
+	UpdateStats(hWnd);
 }
